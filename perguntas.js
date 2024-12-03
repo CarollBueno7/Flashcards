7488e7686fe2f75cec8fd33259dcfebbf335a7c1
Maria Carolina <carolbcarriel17@gmail.com>
2024-12-03 00:19:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Biologia',
+    'Como os processos de mitose e meiose se diferenciam?',
+    'Enquanto a mitose ocorre nas células somáticas e produz 2 células-filhas diplóides (2n), a meiose acontece nas células germinativas e gera 4 células-filhas haploides (n).'
+)
+
+criaCartao(
+    'Português',
+    'O que é conceptismo?',
+    'O conceptismo, também chamado de Quevedismo, é uma vertente literária do barroco que brinca com as ideias através de argumentos e raciocínio lógico, valorizando, essencialmente, o conteúdo do texto.'
+)
+
+criaCartao(
+    'Física',
+    'O que são radioisótopos?',
+    'Radioisótopos são isótopos (elementos com o mesmo número atômico) que emitem radiação.'
+)
+
+criaCartao(
+    'Química',
+    'O que é um carbono saturado?',
+    'O carbono saturado é aquele que contêm apenas ligações simples entre carbonos.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os países-membros do G7?',
+    'Os países que compõem o G7 são: Estados Unidos, Canadá, Alemanha, França, Reino Unido, Itália e Japão.'
+)
+
+criaCartao(
+    'História',
+    'Por que a abrtura dos portos do Brasil em 1808 foi importante?',
+    'Porque isso permitiu que a economia brasileira se tornasse autônoma.'
+)
